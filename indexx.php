46bad48ebf0f28bcdc404795cb1656530e720057
AKO TO <134520855+patsuuu@users.noreply.github.com>
2023-07-02 18:57:02 +0800

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+</head>
+<a href="index.php" class="btn btn-success btn-sm">Logout</a>
+<body>
+
+    <section id="Home">
+        <nav>
+            <div class="logo">
+                <img src="https://media.discordapp.net/attachments/1028865757444055092/1123563414510370906/1519951680038.png">
+            </div>
+
+            <ul>
+                <li><a href="#Home">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="data.php">Photos</a></li>
+                <li><a href="videos.php">Videos</a></li>
+                <li><a href="bar.php">Data</a></li>
+                <li><a href="indexxx.php">Student Details</a></li>
+               
+            </ul>
+
+
+
+        </nav>
+
+        <div class="main">
+
+            <div class="men_text">
+                <h1>Cavite<span>State</span><br>University</h1>
+            </div>
+
+            <div class="main_image">
+                <img src="https://media.discordapp.net/attachments/1028865757444055092/1123564962632171530/laya-at-diwa-cavite-state-university.png?width=267&height=467">
+            </div>
+
+        </div>
+
+       
+
+       
+
+
+
+
+    
+</body>
+</html>
